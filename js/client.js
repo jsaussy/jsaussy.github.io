d622583842f36a1a1f2c5c3eeccb2edca275e544
jsaussy <135028340+jsaussy@users.noreply.github.com>
2025-06-06 11:15:43 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -2,62 +2,84 @@
 
 var Promise = TrelloPowerUp.Promise;
 
-
-TrelloPowerUp.initialize({
-	'card-buttons': function(t, options) {
-		return [{
-			text: 'Open confirm',
-			        callback: function (t) {
-          			  return t.popup({
-            			    title: "Confirm",
-            			    type: 'confirm',
-				    message: 'Confirm?',
-				    confirmText: 'Confirm!',
-				    onConfirm: () => {console.log('confirm');},
-         			   });
-				},
-		}];
-	},
-		'board-buttons': function(t, options) {
-		return [{
-			text: 'Open confirm',
-			        callback: function (t) {
-          			  return t.popup({
-            			    title: "Confirm",
-            			    type: 'confirm',
-				    message: 'Confirm?',
-				    confirmText: 'Confirm!',
-				    onConfirm: () => {console.log('confirm');},
-         			   });
-				},
-		},
-			{
-				text: 'Open list',
-				callback: function (t) {
-					return t.popup({
-						title: 'List!',
-						items: [
-						{
-							text: 'Item 1',
-							callback: function (t, opts) {
-								console.log('Item 1');
-							}
-						},
-												{
-							text: 'Item 2',
-							callback: function (t, opts) {
-								console.log('Item 2');
-							}
-						},
-												{
-							text: 'Item 3',
-							callback: function (t, opts) {
-								console.log('Item 3');
-							}
-						}
-						],
-					});
-				},
-			}];
-	},
-}, {targetOrigin: "https://trellis.coffee"});
+TrelloPowerUp.initialize(
+  {
+    "card-buttons": function (t, options) {
+      return [
+        {
+          text: "Open confirm",
+          callback: function (t) {
+            return t.popup({
+              title: "Confirm",
+              type: "confirm",
+              message: "Confirm?",
+              confirmText: "Confirm!",
+              onConfirm: () => {
+                console.log("confirm");
+              },
+            });
+          },
+        },
+        {
+          text: "Open date",
+          callback: function (t) {
+            t.popup({
+              type: "date",
+              title: "DaTE!!!!",
+              callback: function (t, opts) {
+                console.log(opts.date);
+              },
+            });
+          },
+        },
+      ];
+    },
+    "board-buttons": function (t, options) {
+      return [
+        {
+          text: "Open confirm",
+          callback: function (t) {
+            return t.popup({
+              title: "Confirm",
+              type: "confirm",
+              message: "Confirm?",
+              confirmText: "Confirm!",
+              onConfirm: () => {
+                console.log("confirm");
+              },
+            });
+          },
+        },
+        {
+          text: "Open list",
+          callback: function (t) {
+            return t.popup({
+              title: "List!",
+              items: [
+                {
+                  text: "Item 1",
+                  callback: function (t, opts) {
+                    console.log("Item 1");
+                  },
+                },
+                {
+                  text: "Item 2",
+                  callback: function (t, opts) {
+                    console.log("Item 2");
+                  },
+                },
+                {
+                  text: "Item 3",
+                  callback: function (t, opts) {
+                    console.log("Item 3");
+                  },
+                },
+              ],
+            });
+          },
+        },
+      ];
+    },
+  },
+  { targetOrigin: "https://trellis.coffee" }
+);
