0fc0f0c3dc8e43028a21efffdac33254da45e0a4
lren <lren@atlassian.com>
2025-06-20 14:48:05 -0400

    reformat

--- js/client.js
+++ js/client.js
@@ -20,7 +20,7 @@ TrelloPowerUp.initialize(
             });
           },
         },
-        	       {
+        {
           text: "Open date",
           callback: function (t) {
             t.popup({
@@ -29,8 +29,8 @@ TrelloPowerUp.initialize(
               callback: function (t, opts) {
                 console.log(opts.date);
               },
-		    minDate: new Date("2025-03-01"),
-		     maxDate: new Date()
+              minDate: new Date("2025-03-01"),
+              maxDate: new Date(),
             });
           },
         },
@@ -86,51 +86,56 @@ TrelloPowerUp.initialize(
             });
           },
         },
-	       {
-            text: "Open board bar",
-            callback: function(t) {
-                 return t.boardBar({
-      // required URL to load in the iframe
-      url: './iframe.html',
-      // optional arguments to be passed to the iframe as query parameters
-      // access later with t.arg('text')
-      args: { text: 'Hello' },
-      // optional color for header chrome
-      accentColor: '#F2D600',
-      // initial height for iframe
-      height: 200, // initial height for iframe
-      // optional function to be called if user closes modal
-      callback: () => console.log('Goodbye.'),
-      // optional boolean for whether the user should
-      // be allowed to resize the bar vertically
-      resizable: true,
-      // optional title for header chrome
-      title: 'Board Meeting',
-      // optional action buttons for header chrome
-      // max 3, up to 1 on right side
-      actions: [{
-        icon: './images/icon.svg',
-        url: 'https://google.com',
-        alt: 'Leftmost',
-        position: 'left',
-      }, {
-        icon: './images/icon.svg',
-        callback: (tr) => tr.popup({
-          title: tr.localizeKey('appear_in_settings'),
-          url: 'settings.html',
-          height: 164,
-        }),
-        alt: 'Second from left',
-        position: 'left',
-      }, {
-        icon: './images/icon.svg',
-        callback: () => console.log(':tada:'),
-        alt: 'Right side',
-        position: 'right',
-      }],
-    });
-            }
-        }
+        {
+          text: "Open board bar",
+          callback: function (t) {
+            return t.boardBar({
+              // required URL to load in the iframe
+              url: "js/iframe.html",
+              // optional arguments to be passed to the iframe as query parameters
+              // access later with t.arg('text')
+              args: { text: "Hello" },
+              // optional color for header chrome
+              accentColor: "#F2D600",
+              // initial height for iframe
+              height: 200, // initial height for iframe
+              // optional function to be called if user closes modal
+              callback: () => console.log("Goodbye."),
+              // optional boolean for whether the user should
+              // be allowed to resize the bar vertically
+              resizable: true,
+              // optional title for header chrome
+              title: "Board Meeting",
+              // optional action buttons for header chrome
+              // max 3, up to 1 on right side
+              actions: [
+                {
+                  icon: "./images/icon.svg",
+                  url: "https://google.com",
+                  alt: "Leftmost",
+                  position: "left",
+                },
+                {
+                  icon: "./images/icon.svg",
+                  callback: (tr) =>
+                    tr.popup({
+                      title: tr.localizeKey("appear_in_settings"),
+                      url: "settings.html",
+                      height: 164,
+                    }),
+                  alt: "Second from left",
+                  position: "left",
+                },
+                {
+                  icon: "./images/icon.svg",
+                  callback: () => console.log(":tada:"),
+                  alt: "Right side",
+                  position: "right",
+                },
+              ],
+            });
+          },
+        },
       ];
     },
   },
