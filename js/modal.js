c91caf11d0879c058dac22cf119e651bb2f94482
jsaussy <135028340+jsaussy@users.noreply.github.com>
2025-10-06 10:47:45 -0400

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,11 @@ const t = window.TrelloPowerUp.iframe();
 
   t.lists("all").then(function (lists) {
       console.log(JSON.stringify(lists, null, 2));
-  });
+  }).catch(e => console.error(e));
 
 t.render(() => {
   console.log(t.getContext());
   t.lists("all").then(function (lists) {
       console.log(JSON.stringify(lists, null, 2));
-  });
+  }).catch(e => console.error(e));
 });
