42ee7107c9c3828802bffdd3b2cd04e5576f2438
jsaussy <135028340+jsaussy@users.noreply.github.com>
2025-10-07 22:51:44 -0400

    Create modal-dev.js

---
+++ js/modal-dev.js
@@ -0,0 +1,12 @@
+const t = window.TrelloPowerUp.iframe({targetOrigin: "http://localhost:2999/"});
+
+  t.lists("all").then(function (lists) {
+      console.log(JSON.stringify(lists, null, 2));
+  }).catch(e => console.error(e));
+
+t.render(() => {
+  console.log(t.getContext());
+  t.lists("all").then(function (lists) {
+      console.log(JSON.stringify(lists, null, 2));
+  }).catch(e => console.error(e));
+});
