dfac908799d442f40a5989b489b6b86e29b9fec7
jsaussy <135028340+jsaussy@users.noreply.github.com>
2025-10-08 13:21:12 -0400

    Update panel.js

--- js/panel.js
+++ js/panel.js
@@ -1,5 +1,13 @@
 const t = window.TrelloPowerUp.iframe({targetOrigin: "http://localhost:2999/"});
 
   t.lists("all").then(function (lists) {
-      console.log(JSON.stringify(lists, null, 2));
+      document.getElementById('list').innerHTML = "Here's a list of cards on this board:";
+      lists.map(({list}) => {
+        list.cards.map(({card}) => {
+          const cardEl = document.createElement('div');
+          cardEl.appendChild(document.createTextNode(card.name));
+          document.getElementById('list').appendChild(cardEl);
+        }
+      }
+    );
   }).catch(e => console.error(e));
