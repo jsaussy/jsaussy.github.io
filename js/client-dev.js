d0c666e4dd079272019c49d9ee84048fc0a41b58
jsaussy <135028340+jsaussy@users.noreply.github.com>
2026-01-09 16:33:42 -0500

    Update client-dev.js

--- js/client-dev.js
+++ js/client-dev.js
@@ -21,6 +21,31 @@ function testButton(trello) {
 
 TrelloPowerUp.initialize(
   {
+      "list-sorters": function (t) {
+    return t.list('name', 'id')
+    .then(function (list) {
+      return [{
+        text: "Card Name",
+        callback: function (t, opts) {
+          // Trello will call this if the user clicks on this sort
+          // opts.cards contains all card objects in the list
+          var sortedCards = opts.cards.sort(
+            function(a,b) {
+              if (a.name > b.name) {
+                return 1;
+              } else if (b.name > a.name) {
+                return -1;
+              }
+              return 0;
+            });
+
+          return {
+            sortedIds: sortedCards.map(function (c) { return c.id; })
+          };
+        }
+      }];
+    });
+  },
       "attachment-sections": function(t, options){
     var claimed = options.entries.filter(function (attachment) {
       return attachment.url.indexOf('https://www.nps.gov/yell/') === 0;
