026c5e85909432e097b512acb0e16b2c42f2e5f0
jsaussy <135028340+jsaussy@users.noreply.github.com>
2026-01-14 12:59:20 -0500

    Create popupIframe.js

---
+++ js/popupIframe.js
@@ -0,0 +1,19 @@
+/* global TrelloPowerUp */
+
+
+const t = window.TrelloPowerUp.iframe({targetOrigin: "http://localhost:2999/"});
+
+t.render(() => {
+  document.getElementById("buttonToClick").addEventListener('click', (event) => {
+    t.popUp({
+        type: 'confirm',
+        title: 'Confirm?',
+        message: 'this is a confirm popup',
+        confirmText: 'Confirm',
+        onConfirm: function(t, opts) {
+          console.log('CONFIRM');
+        },
+        confirmStyle?: 'primary'
+    });
+  });
+});
