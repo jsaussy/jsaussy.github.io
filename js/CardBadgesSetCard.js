a79dd3289add93ea54f481f1ffdfcae73dc628a8
jsaussy <135028340+jsaussy@users.noreply.github.com>
2026-07-17 11:19:21 -0400

    Update CardBadgesSetCard.js

--- js/CardBadgesSetCard.js
+++ js/CardBadgesSetCard.js
@@ -1,8 +1,17 @@
 const targetOrigin = document.referrer === 'http://localhost:2999' ? document.referrer : 'https://trellis.coffee/';
 
 const cardBadges = function (t, opts) {
-  const rand = (Math.random() * 100).toFixed(0).toString();
-  t.set('card', 'shared', 'random', rand);
+              return [{
+                dynamic: function() {
+                    const rand = (Math.random() * 100).toFixed(0).toString();
+                    t.set('card', 'shared', 'random', rand);
+                    return {
+                        text: 'badge',
+                        icon: "https://jsaussy.github.io/icon-gray.svg",
+                        color: "red",
+                    };
+                }
+            }];
 };
 
 TrelloPowerUp.initialize(
